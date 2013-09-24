c3056b25e42de9f1d3d3e4c327f0119c9014acf2
euphonael <adonis.ajehian@gmail.com>
2013-09-24 19:51:49 +0700

    add admin form

--- application/views/admin/user/add_user.php
+++ application/views/admin/user/add_user.php
@@ -13,13 +13,75 @@
         </div>
         
         <div id="form-content">
-        	<fieldset>
-            	<legend>Login Details</legend>
-                <p>
-                	<label for="admin_username">Username</label>
-                    <input type="text" name="admin_username" id="admin_username" class="required" />
-                </p>
-            </fieldset>
+        	<div id="form-left">
+                <fieldset>
+                    <legend>Login Details</legend>
+                    <p>
+                        <label class="label-input" for="admin_username">Username</label>
+                        <input type="text" name="admin_username" id="admin_username" class="required" />
+                    </p>
+                    <p>
+                        <label class="label-input" for="admin_password">Password</label>
+                        <input type="password" name="admin_password" id="admin_password" class="required" />
+                    </p>
+                </fieldset>
+                
+                <fieldset>
+                    <legend>Personal Details</legend>
+                    <p>
+                        <label class="label-input" for="admin_name">Name</label>
+                        <input type="text" name="admin_name" id="admin_name" class="required" />
+                    </p>
+                    <p>
+                        <label class="label-input" for="admin_dob">Date of Birth</label>
+                        <input type="text" name="admin_dob" id="admin_dob" class="required" />
+                    </p>
+                    <p>
+                        <label class="label-input" for="admin_pob">Place of Birth</label>
+                        <input type="text" name="admin_pob" id="admin_pob" class="required" />
+                    </p>
+                    <p>
+                        <label class="label-input" for="admin_phone">Phone</label>
+                        <input type="text" name="admin_phone" id="admin_phone" class="required digits" />
+                    </p>
+                    <p>
+                        <label class="label-input" for="admin_personal_email">Personal E-mail</label>
+                        <input type="text" name="admin_personal_email" id="admin_email" class="required email" />
+                    </p>
+                    <p>
+                        <label class="label-input" for="admin_ktp">Scan KTP</label>
+                        <input type="file" name="admin_ktp" id="admin_ktp" class="required" />
+                    </p>
+                    <p>
+                        <label class="label-input" for="admin_npwp">Scan NPWP</label>
+                        <input type="file" name="admin_npwp" id="admin_npwp" />
+                    </p>
+                </fieldset>
+            </div>
+            
+            <div id="form-right">
+                <fieldset>
+                    <legend>Staff Info</legend>
+                    <p>
+                        <label class="label-input" for="admin_work_email">Work E-mail</label>
+                        <input type="text" name="admin_work_email" id="admin_work_email" class="required email" />
+                    </p>
+                    <p>
+                        <label class="label-input" for="admin_job_position">Job Position</label>
+                        <input type="text" name="admin_job_position" id="admin_job_position" class="required" />
+                    </p>
+                    <p>
+                        <label class="label-input" for="admin_join_date">Join Date</label>
+                        <input type="text" name="admin_join_date" id="admin_join_date" class="required" />
+                    </p>
+                    <p>
+                        <label class="label-input" for="admin_resign_date">Resign Date</label>
+                        <input type="text" name="admin_resign_date" id="admin_resign_date" class="required" />
+                    </p>
+                </fieldset>
+            </div>
+            
+            <div class="clear"></div>
         </div>
     </form>
 </div>
--- js/admin/form.js
+++ js/admin/form.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
+
+	/* Form Auto Width */
+	var max_width = 450;
+	var label_width = 0;
+	
+	$('#form-content label.label-input').each(function(){
+		if ($(this).width() > label_width)
+		{
+			label_width = $(this).width();
+		}
+	});
+		
+	$('#form-content label.label-input').width(label_width);
+	
+	var field_width = 0;
+	
+	$('#form-content input').each(function(){
+		var input_width = max_width - label_width - 40;
+		$('#form-content input').width(input_width);
+	});
+	/* END Form Auto Width */
 	
 	$('form#process-data').validate();
 	
